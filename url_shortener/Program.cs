fa388280d14f0e0cae6029f7f0882e020c317482
idolatz <idolatz@gmail.com>
2025-06-12 01:47:56 +0300

    a

--- url_shortener/Program.cs
+++ url_shortener/Program.cs
@@ -1,50 +1,16 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace UrlShortenerApp
 {
     class Program
     {
         static void Main(string[] args)
-        {   
-            string serverIP = "127.0.0.1";
-            UrlMgmt um = new UrlMgmt(serverIP);
-
-            string message = """
-                help            print this message
-                getCode [url    create or get a shortcut
-                getURL [Code]   see the url of that code
-                list            list all shortcuts
-                delete [url]    delete shortcut by shortcut
-                """;
-            if (args.Length == 0)
-            {
-                Console.WriteLine(message);
-                return;
-            }
-            else if (args[0] == "getCode" && args.Length == 2)
-            {
-                Console.WriteLine(um.GetShortenUrl(args[1]));
-            }
-            else if (args[0] == "getURL" && args.Length == 2)
-            {
-                um.GetUrlFromCode(args[1]);
-            }
-            else if (args[0] == "list" && args.Length == 1)
-            {
-                um.ListAll();
-            }
-            else if (args[0] == "delete" && args.Length == 2)
-            {
-                um.DeleteURL(args[1]);
-            }
-            else
-            {
-                Console.WriteLine(message);
-                return;
-            }
-
-
+        {
+            //netsh http add urlacl url = http://IP:port/ user=DOMAIN\user
+            HTTPServer server = new HTTPServer("+:8080");
 
         }
     }
 }
+
--- url_shortener/myClasses.cs
+++ url_shortener/myClasses.cs
@@ -1,6 +1,10 @@
-﻿#nullable disable
+﻿using Microsoft.Data.Sqlite;
+using Sprache;
 using System;
-using Microsoft.Data.Sqlite;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+using System.Web;
 
 public class DbHandler
 {
@@ -120,21 +124,18 @@ public class DbHandler
 
 public class UrlMgmt
 {
-    public string serverUrl;
     private DbHandler db;
-    public UrlMgmt(string serverUrl)  
+    public UrlMgmt()  
     {
         // initialize the UrlMgmt Object
         this.db = new DbHandler();
-        this.serverUrl = serverUrl;
     }
-    public string GetShortenUrl(string originalUrl)
+    public string GetCode(string originalUrl)
     {
         // Generate a unique code for the shortened URL
         string code = GenerateShortCode(originalUrl);
         //build the shorten url
-        string shortenUrl = "http://" + this.serverUrl + "/" + code;
-        return shortenUrl;
+        return code;
     }
 
     public void ListAll()
@@ -153,13 +154,13 @@ public class UrlMgmt
         Console.WriteLine("DONE!");
     }
 
-    public void GetUrlFromCode(string code)
+    public string GetUrlFromCode(string code)
     {
         if (!this.db.CheckCodeExists(code)) {
             Console.WriteLine("Code is not exists");
-            return;
+            return "***not-found***";
         }
-        Console.WriteLine(this.db.GetUrlFromCode(code));
+        return this.db.GetUrlFromCode(code);
     }
 
 
@@ -201,4 +202,112 @@ public class UrlMgmt
     }
 
 
+}
+
+
+public class HTTPServer
+{
+    private HttpListener listener;
+    private UrlMgmt um;
+    private string prefix;
+    public HTTPServer(string prefix)
+    {
+        this.listener = new HttpListener();
+        this.um = new UrlMgmt();
+        this.prefix = prefix;
+        this.listener.Prefixes.Add($"http://{this.prefix}/");
+        this.listener.Start();
+        Console.WriteLine($"Listening on http://{this.prefix}/");
+
+
+        while (true)
+        {
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            string requestedUrl = request.RawUrl;
+            Console.WriteLine($"Request for {requestedUrl}");
+
+            if (requestedUrl.StartsWith("/?url="))
+            {
+                string originalUrl = extractVar(requestedUrl, "url");
+                string code = this.um.GetCode(originalUrl);
+                string shortenUrl = $"http://{this.prefix}/{code}";
+
+                string responseString = $"<html><body><h1>your shorten url is: <br>{shortenUrl} </h1></body></html>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+                response.Close();
+            }
+
+            else if (requestedUrl.StartsWith("/?code="))
+            {
+                string code = extractVar(requestedUrl, "code");
+                string orginalUrl = this.um.GetUrlFromCode(code);
+                string responseString = $"<html><body><h1>checking code {code} <br> answer: <br> {orginalUrl}</h1></body></html>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+                response.Close();
+            }
+
+            else if (requestedUrl.Length == 7)
+            {
+                // Redirect
+                string code = requestedUrl.TrimStart('/');
+                string originalUrl = this.um.GetUrlFromCode(code);
+                
+                response.StatusCode = (int)HttpStatusCode.Redirect; // 302
+
+                if (originalUrl == "***not-found***")
+                {
+                    response.RedirectLocation = "/code-not-exists";
+                    Console.WriteLine("redirected to: /code-not-exists");
+                }
+                else
+                {
+                    response.RedirectLocation = originalUrl;
+                    Console.WriteLine("redirected to: " + originalUrl);
+                }
+                response.Close();
+            }
+
+            else if (requestedUrl == "/code-not-exists")
+            {
+                string responseString = "<html><body><h1>code is not exists</h1></body></html>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+                response.Close();
+            }
+
+            else
+            {
+                Console.WriteLine("redirected to: /code-not-exists");
+                response.StatusCode = (int)HttpStatusCode.Redirect;
+                response.RedirectLocation = "/code-not-exists";
+                response.Close();
+            }
+        }
+
+    }
+
+    private string extractVar(string url, string type)
+    {
+        // Create a Uri with a dummy host (required by HttpUtility)
+        var uri = new Uri("http://temp" + url);
+
+        var query = HttpUtility.ParseQueryString(uri.Query);
+        string urlParam = query.Get(type);
+
+        // Optional: remove surrounding quotes if present
+        if (urlParam != null && urlParam.StartsWith("\"") && urlParam.EndsWith("\""))
+        {
+            urlParam = urlParam.Substring(1, urlParam.Length - 2);
+        }
+
+        return urlParam;
+    }
 }
